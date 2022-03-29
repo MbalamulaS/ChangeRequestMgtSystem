7db6e2108fdb9523306c44ced73846dad12bad62
MbalamulaS <nyanditi.mbalamula@gmail.com>
2022-03-29 12:23:30 +0300

    database arctect maintained

--- change/request/forms/config/database.php
+++ change/request/forms/config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'mandems'),
+            'database' => env('DB_DATABASE', 'mnrtcrf'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', 'MNRTweb@2021'),
             'charset' => 'utf8',
